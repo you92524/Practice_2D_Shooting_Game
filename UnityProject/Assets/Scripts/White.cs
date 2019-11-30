2f2fa3e1b6e73de5d574fd8a95024c203ed776fc
KID421 <suchild7@gmail.com>
2019-11-30 12:07:08 +0800

    練習版

--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,27 +3,7 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    [Header("子彈")]
-    public GameObject bullet;
-    [Header("子彈發射位置")]
-    public Transform point;
-    [Header("音效區域")]
-    public AudioSource aud;
-    public AudioClip soundShoot;
-
-    private void Update()
-    {
-        Shoot();
-    }
-
-    private void Shoot()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            aud.PlayOneShot(soundShoot, 1.5f);
-            Instantiate(bullet, point.position, Quaternion.identity);
-        }
-    }
+    
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
